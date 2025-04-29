e6b569f7d0917a57d3dc4b1bb849f703aed5eeee
JustNormalProgrammer <testkonto1290@gmail.com>
2025-04-29 22:35:20 +0200

    Inital implementation of auth context provider

--- frontend/src/routes/__root.jsx
+++ frontend/src/routes/__root.jsx
@@ -1,55 +1,16 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import Navbar from "../components/navbar/Navbar";
 export const Route = createRootRoute({
-  component: () => (
+  component: rootComponent,
+});
+
+function rootComponent() {
+  return (
     <>
-      <nav>
-        <div className="nav-link-list">
-          <Link
-            to="/"
-            activeProps={{
-              style: {
-                color: "rgb(255, 0, 234)",
-              },
-            }}
-          >
-            Home
-          </Link>
-          <Link
-            to="/posts"
-            activeProps={{
-              style: {
-                color: "rgb(255, 0, 234)",
-              },
-            }}
-          >
-            Posts
-          </Link>
-          <Link
-            to="/login"
-            activeProps={{
-              style: {
-                color: "rgb(255, 0, 234)",
-              },
-            }}
-          >
-            Login
-          </Link>
-          <Link
-            to="/signup"
-            activeProps={{
-              style: {
-                color: "rgb(255, 0, 234)",
-              },
-            }}
-          >
-            Signup
-          </Link>
-        </div>
-      </nav>
-      <hr />
+      <Navbar />
       <Outlet />
       <TanStackRouterDevtools />
     </>
-  ),
-});
+  );
+}
--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,13 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/')({
+import { createFileRoute } from "@tanstack/react-router";
+import { useAuth } from "../contexts/authProvider";
+export const Route = createFileRoute("/")({
   component: Index,
-})
+});
 
 function Index() {
+  const auth = useAuth();
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  )
+    <>
+      <h2>Welcome to Home Page! </h2>
+      {auth.userName && <h3>Welcome {auth.userName}!</h3>}
+    </>
+  );
 }
