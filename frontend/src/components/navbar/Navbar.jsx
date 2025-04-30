3325d2fc125828a259594ec231e3ee8d8e1570bb
jlewa <jlewandowska334@gmail.com>
2025-04-30 17:51:43 +0200

    Improve navbar and remove index.css file

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,14 +1,35 @@
 import { useAuth } from "../../contexts/authProvider";
+import { useLocation } from "@tanstack/react-router";
+import { useEffect } from "react";
+
 export default function Navbar() {
   const auth = useAuth();
+  const location = useLocation();
+  const isMainPage = location.pathname === "/";
+
+  useEffect(() => {
+    document.body.className = isMainPage ? "" : "main-page";
+  }, [isMainPage]);
+
   return (
-      <nav
-        className="d-flex justify-content-end align-items-center"
-        style={{
-          paddingRight: "2vw",
-          marginTop: "4vh",
-        }}
+    <nav
+      className={`d-flex justify-content-between align-items-center navbar ${
+        isMainPage ? "" : "navbar-main"
+      }`}
+    >
+      <div
+        className="left-sidebar"
+        style={{ visibility: isMainPage ? "hidden" : "visible" }}
       >
+        <div className="headline-navbar-title">
+          <i className="bi bi-heart me-2"></i>
+          Vibe {/* TODO: glupio miga */}
+        </div>
+        <div className="navbar-subtitle">
+          We absoluty do not copy reddit
+        </div>
+      </div>
+      <div className="d-flex">
         <button
           className="btn btn-primary btn-register"
           onClick={() => (window.location.href = "/signup")}
@@ -22,7 +43,7 @@ export default function Navbar() {
         >
           Zaloguj
         </button>
-        <button onClick={auth.logOut}>Logout</button>
-      </nav>
+      </div>
+    </nav>
   );
 }
