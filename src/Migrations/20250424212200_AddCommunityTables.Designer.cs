0a4810e6d9c2f77d53a5897278fa1fc2d8fe48a8
patrykzawadzkisggw <115794809+patrykzawadzkisggw@users.noreply.github.com>
2025-04-30 09:56:47 +0200

    add Community (#5)

--- src/Migrations/20250424212200_AddCommunityTables.Designer.cs
+++ src/Migrations/20250424212200_AddCommunityTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Projekt_Zespolowy.Authentication;
 
@@ -11,9 +12,11 @@ using Projekt_Zespolowy.Authentication;
 namespace Projekt_Zespolowy.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250424212200_AddCommunityTables")]
+    partial class AddCommunityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
