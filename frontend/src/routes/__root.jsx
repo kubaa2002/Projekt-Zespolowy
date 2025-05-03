f58eaa9c47934b38be213e9d7a515afee7508b06
jlewa <jlewandowska334@gmail.com>
2025-05-03 17:44:06 +0200

    Improve placement of login/register forms

--- frontend/src/routes/__root.jsx
+++ frontend/src/routes/__root.jsx
@@ -1,16 +1,19 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import Navbar from "../components/navbar/Navbar";
+
 export const Route = createRootRoute({
   component: rootComponent,
 });
 
 function rootComponent() {
   return (
-    <>
+    <div className="root-container">
       <Navbar />
-      <Outlet />
+      <div className="centered-container">
+        <Outlet />
+      </div>
       <TanStackRouterDevtools />
-    </>
+    </div>
   );
 }
