b5dd1d8bd0cfd952d6f9be147bb971269314aedb
JustNormalProgrammer <testkonto1290@gmail.com>
2025-05-08 23:47:19 +0200

    Fix form requests on submit, enhance navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,12 +1,14 @@
-import { useAuth } from "../../contexts/authProvider";
-import { useLocation } from "@tanstack/react-router";
+import { useLocation, useNavigate } from "@tanstack/react-router";
 import { useEffect } from "react";
-
+import { useAuth } from "../../contexts/authProvider";
+import SearchInput from "../primitives/SearchInput";
 export default function Navbar() {
-  const auth = useAuth();
   const location = useLocation();
   const isMainPage = location.pathname === "/";
-
+  const navigate = useNavigate();
+  const auth = useAuth();
+  // I think we should create 2 navbars. One for each state loggedin/loggedout. Now the add Post button is visible on the hero page
+  // We have to decide whether main-page is at "/" and it's structure depends on login status, or at different endpoint eg. "/hero"
   useEffect(() => {
     document.body.className = isMainPage ? "" : "main-page";
   }, [isMainPage]);
@@ -23,27 +25,41 @@ export default function Navbar() {
       >
         <div className="headline-navbar-title">
           <i className="bi bi-heart me-2"></i>
-          Vibe {/* TODO: glupio miga */}
+          Vibe
         </div>
-        <div className="navbar-subtitle">
-          We absoluty do not copy reddit
+        <div className="navbar-subtitle">We absoluty do not copy reddit</div>
+      </div>
+      {auth.isAuthenticated() ? (
+        <div className="d-flex flex-grow-1" style={{gap: '15px'}}>
+          <SearchInput/>
+          <button className="btn btn-primary btn-register">
+          <i className="bi bi-plus-circle me-2"></i>
+          Nowy post</button>
+          <button
+            className="btn btn-danger"
+            onClick={() => auth.logOut()}
+          >
+            Wyloguj
+          </button>
         </div>
-      </div>
-      <div className="d-flex">
-        <button
-          className="btn btn-primary btn-register"
-          onClick={() => (window.location.href = "/signup")}
-        >
-          <i className="bi bi-people me-2"></i>
-          Rejestracja
-        </button>
-        <button
-          className="btn btn-primary btn-login"
-          onClick={() => (window.location.href = "/login")}
-        >
-          Zaloguj
-        </button>
-      </div>
+        
+      ) : (
+        <div className="d-flex">
+          <button
+            className="btn btn-primary btn-register"
+            onClick={() => navigate({ to: "/signup" })}
+          >
+            <i className="bi bi-people me-2"></i>
+            Rejestracja
+          </button>
+          <button
+            className="btn btn-primary btn-login"
+            onClick={() => navigate({ to: "/login" })}
+          >
+            Zaloguj
+          </button>
+        </div>
+      )}
     </nav>
   );
 }
