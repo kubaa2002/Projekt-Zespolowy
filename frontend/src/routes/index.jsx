d9e7343fa6606ccf9f452fccb3e67f25547493dc
jlewa <jlewandowska334@gmail.com>
2025-05-12 16:03:23 +0200

    Implement authentication-based navbar, login redirect and hero section
    Show navbar based on auth status
    
    Redirect to /login if not authenticated
    
    Add /hero section

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { useAuth } from "../contexts/authProvider";
 export const Route = createFileRoute("/")({
   component: Index,
@@ -7,9 +7,9 @@ export const Route = createFileRoute("/")({
 function Index() {
   const auth = useAuth();
   return (
-    <>
-      <h2>Welcome to Home Page! </h2>
+    <div>
+      <h2>Welcome to Home Page!</h2>
       {auth.userName && <h3>Welcome {auth.userName}!</h3>}
-    </>
+    </div>
   );
 }
