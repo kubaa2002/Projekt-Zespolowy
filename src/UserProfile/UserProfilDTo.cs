bfecd3fc45c26cce1cb7753259b568d35d2244a7
PWelkier <149411241+PWelkier@users.noreply.github.com>
2025-05-25 19:26:22 +0200

    add UserProfile

---
+++ src/UserProfile/UserProfilDTo.cs
@@ -0,0 +1,15 @@
+﻿namespace Projekt_Zespolowy.UserProfile
+{
+    public class UserProfileDTo
+    {
+        public string Id { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public DateTimeOffset CreatedAt { get; set; }
+        public bool IsMe { get; set; }
+
+        public int FollowersCount { get; set; }
+        public int FollowingCount { get; set; }
+        public int CommunitiesCount { get; set; }
+    }
+}
+    
