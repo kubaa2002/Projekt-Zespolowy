352c47cdb97a6e08c04f3c5facefa7dda889a91d
jlewa <jlewandowska334@gmail.com>
2025-05-27 19:00:18 +0200

    Move modals and sidenav to separate folders with dedicated scss files

--- frontend/src/components/posts/Posts.jsx
+++
@@ -1,8 +0,0 @@
-export default function Posts() {
-  return (
-    <div>
-      <h1>Posts</h1>
-      <p>List of posts will be displayed here.</p>
-    </div>
-  );
-}
--- frontend/src/routes/posts.jsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/posts')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/post"!</div>
-}
