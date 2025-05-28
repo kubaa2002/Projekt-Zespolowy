1653da92f8fb945ab421831545f68f084c59aec6
jlewa <jlewandowska334@gmail.com>
2025-05-28 02:11:41 +0200

    Modify modals for community and post creation

--- frontend/src/components/create/Create.jsx
+++ frontend/src/components/create/Create.jsx
@@ -1,12 +1,32 @@
 import { useLocation, useNavigate } from "@tanstack/react-router";
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
 import MainLayout from "../main/MainLayout";
+import MainModal from "../modals/MainModal";
 
 export default function Create() {
     const location = useLocation();
     const navigate = useNavigate();
     const params = new URLSearchParams(location.search);
+    const maxLength = 2000;
+    const [postContent, setPostContent] = useState("");
+    const [communityContent, setCommunityContent] = useState("");
+    const [postFile, setPostFile] = useState(null);
+    const [communityFile, setCommunityFile] = useState(null);
+    const fileInputRef = useRef(null);
     const type = params.get("type");
+
+    const handleFileChange = (e) => {
+        if (type === "post") setPostFile(e.target.files[0]);
+        else setCommunityFile(e.target.files[0]);
+    };
+
+    const handleRemove = () => {
+        if (type === "post") setPostFile(null);
+        else setCommunityFile(null);
+        if (fileInputRef.current) {
+            fileInputRef.current.value = "";
+        }
+    };
 
     useEffect(() => {
         if (!type) {
@@ -19,16 +39,31 @@ export default function Create() {
             <div className="centered-container">
                 <div className="create-container">
                     {type === "post" && (
-                        <div>
-                            <h2>Nowy post</h2>
-                            {/* Formularz posta */}
-                        </div>
+                        <MainModal
+                        show={true}
+                        onClose={() => setRotated(false)}
+                        maxLength={maxLength}
+                        content={postContent}
+                        setContent={setPostContent}
+                        file={postFile}
+                        handleFileChange={handleFileChange}
+                        handleRemove={handleRemove}
+                        fileInputRef={fileInputRef}
+                        />
                     )}
                     {type === "community" && (
-                        <div>
-                            <h2>Stwórz społeczność</h2>
-                            {/* Formularz społeczności */}
-                        </div>
+                        <MainModal
+                        show={true}
+                        onClose={() => setRotated(false)}
+                        maxLength={maxLength}
+                        content={communityContent}
+                        setContent={setCommunityContent}
+                        file={communityFile}
+                        handleFileChange={handleFileChange}
+                        handleRemove={handleRemove}
+                        fileInputRef={fileInputRef}
+                        hideSelect={true}
+                        />
                     )}
                 </div>
             </div>
