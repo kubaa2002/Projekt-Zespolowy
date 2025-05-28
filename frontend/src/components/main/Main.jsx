cd0007a4e7fcabd3389c1068ac643ecd9dc6ef7b
jlewa <jlewandowska334@gmail.com>
2025-05-28 03:02:13 +0200

    Delete unnecessary code

--- frontend/src/components/main/Main.jsx
+++ frontend/src/components/main/Main.jsx
@@ -25,7 +25,7 @@ export default function Main() {
 
   return (
     <MainLayout>
-                <div className={`main-think${rotated ? " main-think-open" : ""}`}>
+        <div className={`main-think${rotated ? " main-think-open" : ""}`}>
           <img src="avatar.svg" alt="Avatar" className="avatar" />
           <span className="text-think">
             Podziej się tym, co masz na myśli
