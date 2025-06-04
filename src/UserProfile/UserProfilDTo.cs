9e4e0c049748eac590b3c3231351f97d6541d60f
PWelkier <149411241+PWelkier@users.noreply.github.com>
2025-06-04 21:48:42 +0200

    UserProfileController (#17)
    * Add files via upload
    
    * updated UserProfileController
    
    * Update UserProfileController.cs- isFollower
    
    * Update UserProfilDTo.cs
    
    * Delete src/Models/UserProfile.cs
    
    ---------
    
    Co-authored-by: Jakub Zawadzki <93218731+kubaa2002@users.noreply.github.com>

--- src/UserProfile/UserProfilDTo.cs
+++ src/UserProfile/UserProfilDTo.cs
@@ -6,7 +6,8 @@
         public string UserName { get; set; } = string.Empty;
         public DateTimeOffset CreatedAt { get; set; }
         public bool IsMe { get; set; }
-
+        public bool IsFollowing { get; set; }
+        public bool IsFollower { get; set; }
         public int FollowersCount { get; set; }
         public int FollowingCount { get; set; }
         public int CommunitiesCount { get; set; }
