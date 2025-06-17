885fb3ae4ee6d0fa60a3de659d00d701543130cc
kubaa2002 <kubaa20021@proton.me>
2025-06-17 15:08:34 +0200

    Fix to the user profiles posts

--- src/Services/PostsService.cs
+++ src/Services/PostsService.cs
@@ -164,7 +164,10 @@ namespace Projekt_Zespolowy.Services
         }
         public ServiceResponse<List<Post>> GetPostsFromRangeFromUser(int start, int length, string authorId)
         {
-            List<Post> foundPosts = context.Posts.Where(p => !p.IsDeleted).Where(x => x.ParentId == null).Where(x => x.CommunityId == null).Where(x => x.AppUserId == authorId).Include(x => x.Likes).ThenInclude(l => l.Reaction).Include(p => p.Author).Include(p => p.Replies).ToList();
+            var authorPostsIds = context.Posts.Where(p => !p.IsDeleted).Where(x => x.ParentId == null).Where(x => x.AppUserId == authorId).Select(p => p.Id);
+            var sharedPostsIds = context.Shares.Where(sp => sp.AppUserId == authorId).Include(sp => sp.Post).Where(sp => !sp.Post!.IsDeleted).Where(sp => sp.Post!.ParentId == null).Select(sp => sp.Post!.Id);
+            var foundPostsIds = authorPostsIds.Union(sharedPostsIds).ToList();
+            var foundPosts = context.Posts.Where(p => foundPostsIds.Contains(p.Id)).Include(x => x.Likes).ThenInclude(l => l.Reaction).Include(p => p.Author).Include(p => p.Replies).ToList();
             // When no posts
             if (start > foundPosts.Count)
                 return new ServiceResponse<List<Post>>(StatusCodes.Status204NoContent, null);
